350b685bd57ad23ad5ad6a32b14b5824920839aa
n0thus <n0thus@outlook.fr>
2017-08-19 19:12:28 +0200

    Revert "Added scripts"
    This reverts commit 28b37e808db4cbdbf71b1fe687f822aea6a46d87.

--- AdvancedFuel/es_AdvancedFuel/__resource.lua
+++
@@ -1,9 +0,0 @@
-client_scripts {
-	'config_c.lua',
-	'client.lua',
-	'GUI.lua',
-	'map.lua',
-	'models_c.lua'
-}
-
-server_script 'server.lua'
--- logs/__resource.lua
+++
@@ -1,9 +0,0 @@
-client_scripts {
-	'client.lua',
-	'config.lua',	
-}
-
-server_scripts {
-	'server.lua',
-	'hashToWeaponsName_s.lua',
-}
